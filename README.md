53c4e643feee851e0221104d67350b3f0335ff6b
supercrafter333 <justcoolstuff99@gmail.com>
2021-04-17 18:26:54 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# BetterBan - V1.0.0
+
+**Would you like to ban players for a certain period of time? Then BetterBan is just right for you! Easily ban players for a certain time and configure a bit in `config.yml`.**
+
+I would be very happy if you leave a star for this project.
+
+### Features
+- Ban players easy per time
+- configure something in the `config.yml`
+- Easy2Use
+
+### TODOs
+None
+
+### Bugs - none
+You've found a Bug?
+- Go to [Issues](https://github.com/supercrafter333/BetterBan/issues)
+- Click on [New Issue](https://github.com/supercrafter333/BetterBan/issues/new/choose)
+- Write your bug with all Informations that you have down
+- Send Issue
+- Wait, I where answer you
+
+### Commands
+|**Command**|**Description**|
+|-----------|---------------|
+|`/ban`|Ban a player (this overrides the PMMP Ban commmand)|
+
+### License:
+This Pluginis licensed under the [Apache License 2.0](/LICENSE)! Plugin by supercrafter333!
+
+### Credits
+Owner: supercrafter333
+
+Helper: ---
+
+Icon: ---
+
+Discord-Tag: `supercrafter333#4062`
---
+++ src/supercrafter333/BetterBan/BetterBan.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace supercrafter333\BetterBan;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+use supercrafter333\BetterBan\Commands\BanCommand;
+
+/**
+ * Class BetterBan
+ * @package supercrafter333\BetterBan
+ */
+class BetterBan extends PluginBase
+{
+
+    /**
+     * @var
+     */
+    protected static $instance;
+
+    /**
+     *
+     */
+    public function onEnable()
+    {
+        self::$instance = $this;
+        $this->saveResource("config.yml");
+        $cmdMap = $this->getServer()->getCommandMap();
+        $pmmpBanCmd = $cmdMap->getCommand("ban");
+        $cmdMap->unregister($pmmpBanCmd);
+        $cmdMap->register("BetterBan", new BanCommand("ban"));
+    }
+
+    /**
+     * @return static
+     */
+    public static function getInstance(): self
+    {
+        return self::$instance;
+    }
+
+    /**
+     * @return Config
+     */
+    public function getConfig(): Config
+    {
+        $cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+        return $cfg;
+    }
+
+    /**
+     * @param string $string
+     * @return array|null
+     * @throws \Exception
+     */
+    public function stringToTimestamp(string $string): ?array{
+        /**
+         * Rules:
+         * Integers without suffix are considered as seconds
+         * "s" is for seconds
+         * "m" is for minutes
+         * "h" is for hours
+         * "d" is for days
+         * "w" is for weeks
+         * "mo" is for months
+         * "y" is for years
+         */
+        if(trim($string) === ""){
+            return null;
+        }
+        $t = new \DateTime();
+        preg_match_all("/[0-9]+(y|mo|w|d|h|m|s)|[0-9]+/", $string, $found);
+        if(count($found[0]) < 1){
+            return null;
+        }
+        $found[2] = preg_replace("/[^0-9]/", "", $found[0]);
+        foreach($found[2] as $k => $i){
+            switch($c = $found[1][$k]){
+                case "y":
+                case "w":
+                case "d":
+                    $t->add(new \DateInterval("P" . $i. strtoupper($c)));
+                    break;
+                case "mo":
+                    $t->add(new \DateInterval("P" . $i. strtoupper(substr($c, 0, strlen($c) -1))));
+                    break;
+                case "h":
+                case "m":
+                case "s":
+                    $t->add(new \DateInterval("PT" . $i . strtoupper($c)));
+                    break;
+                default:
+                    $t->add(new \DateInterval("PT" . $i . "S"));
+                    break;
+            }
+            $string = str_replace($found[0][$k], "", $string);
+        }
+        return [$t, ltrim(str_replace($found[0], "", $string))];
+    }
+}
