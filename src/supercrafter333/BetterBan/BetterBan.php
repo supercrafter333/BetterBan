02743576a663c4da5a4a4e8e5c3aa81f63dfdfc2
supercrafter333 <justcoolstuff99@gmail.com>
2021-04-20 19:02:31 +0200

    Recovery 2

---
+++ src/supercrafter333/BetterBan/BetterBan.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace supercrafter333\BetterBan;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+use supercrafter333\BetterBan\Commands\BanCommand;
+use supercrafter333\BetterBan\Commands\BaninfoCommand;
+use supercrafter333\BetterBan\Commands\BanlogCommand;
+
+/**
+ * Class BetterBan
+ * @package supercrafter333\BetterBan
+ */
+class BetterBan extends PluginBase
+{
+
+    /**
+     * @var
+     */
+    protected static $instance;
+
+    public const VERSION = "1.1.0-dev";
+
+    public function onEnable()
+    {
+        self::$instance = $this;
+        $this->saveResource("config.yml");
+        $this->versionCheck(self::VERSION);
+        $cmdMap = $this->getServer()->getCommandMap();
+        $pmmpBanCmd = $cmdMap->getCommand("ban");
+        $cmdMap->unregister($pmmpBanCmd);
+        $cmdMap->registerAll("BetterBan", [
+            new BanCommand("ban"),
+            new BanlogCommand("banlog"),
+            new BaninfoCommand("baninfo")
+        ]);
+    }
+
+    /**
+     * @return static
+     */
+    public static function getInstance(): self
+    {
+        return self::$instance;
+    }
+
+    /**
+     * @return Config
+     */
+    public function getConfig(): Config
+    {
+        $cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+        return $cfg;
+    }
+
+    private function versionCheck($version)
+    {
+        if (!$this->getConfig()->exists("version") || !$this->getConfig()->get("version") == $version) {
+            $this->getLogger()->debug("OUTDATED CONFIG.YML!! You config.yml is outdated! Your config.yml will automatically updated!");
+            unlink($this->getConfig()->getPath());
+            $this->saveResource("config.yml");
+            $this->getConfig()->reload();
+            $this->getLogger()->debug("confg.yml Updated for version: §b$version");
+        }
+    }
+
+    public function getBanLogs()
+    {
+        return new Config($this->getDataFolder() . "banLogs.yml", Config::YAML);
+    }
+
+    public function addBanToBanlog(string $playerName)
+    {
+        $log = $this->getBanLogs();
+        if ($log->exists($playerName)) {
+            $log->set($playerName, intval($log->get($playerName) +1));
+            $log->save();
+        } else {
+            $log->set($playerName, 1);
+            $log->save();
+        }
+    }
+
+    public function getBanLogOf(string $playerName): int
+    {
+        $bans = $this->getBanLogs()->exists($playerName) ? $this->getBanLogs()->get($playerName) : 0;
+        return $bans;
+    }
+
+    /**
+     * @param string $string
+     * @return array|null
+     * @throws \Exception
+     */
+    public function stringToTimestamp(string $string): ?array{
+        /**
+         * Rules:
+         * Integers without suffix are considered as seconds
+         * "s" is for seconds
+         * "m" is for minutes
+         * "h" is for hours
+         * "d" is for days
+         * "w" is for weeks
+         * "mo" is for months
+         * "y" is for years
+         */
+        if(trim($string) === ""){
+            return null;
+        }
+        $t = new \DateTime();
+        preg_match_all("/[0-9]+(y|mo|w|d|h|m|s)|[0-9]+/", $string, $found);
+        if(count($found[0]) < 1){
+            return null;
+        }
+        $found[2] = preg_replace("/[^0-9]/", "", $found[0]);
+        foreach($found[2] as $k => $i){
+            switch($c = $found[1][$k]){
+                case "y":
+                case "w":
+                case "d":
+                    $t->add(new \DateInterval("P" . $i. strtoupper($c)));
+                    break;
+                case "mo":
+                    $t->add(new \DateInterval("P" . $i. strtoupper(substr($c, 0, strlen($c) -1))));
+                    break;
+                case "h":
+                case "m":
+                case "s":
+                    $t->add(new \DateInterval("PT" . $i . strtoupper($c)));
+                    break;
+                default:
+                    $t->add(new \DateInterval("PT" . $i . "S"));
+                    break;
+            }
+            $string = str_replace($found[0][$k], "", $string);
+        }
+        return [$t, ltrim(str_replace($found[0], "", $string))];
+    }
+}
---
+++ src/supercrafter333/BetterBan/Commands/BanCommand.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace supercrafter333\BetterBan\Commands;
+
+use DateInterval;
+use DateTime;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\command\defaults\VanillaCommand;
+use pocketmine\command\utils\InvalidCommandSyntaxException;
+use pocketmine\lang\TranslationContainer;
+use pocketmine\Player;
+use supercrafter333\BetterBan\BetterBan;
+
+/**
+ * Class BanCommand
+ * @package supercrafter333\BetterBan\Commands
+ */
+class BanCommand extends VanillaCommand
+{
+
+    /**
+     * BanCommand constructor.
+     * @param string $name
+     * @param string $description
+     * @param string|null $usageMessage
+     * @param array $aliases
+     */
+    public function __construct(string $name, string $description = "", string $usageMessage = null, array $aliases = [])
+    {
+        parent::__construct($name, "%pocketmine.command.ban.player.description", "§4Use: §r/ban <name> [reason: ...] [date interval: ...]");
+        $this->setPermission("pocketmine.command.ban.player");
+    }
+
+    /**
+     * @param CommandSender $sender
+     * @param string $commandLabel
+     * @param array $args
+     * @return bool
+     */
+    public function execute(CommandSender $sender, string $commandLabel, array $args): bool
+    {
+        $pl = BetterBan::getInstance();
+        $cfg = $pl->getConfig();
+        if (!$this->testPermission($sender)) {
+            return true;
+        }
+
+        if (count($args) < 1) {
+            throw new InvalidCommandSyntaxException();
+        }
+
+        if (count($args) == 2 || count($args) == 1) {
+            $name = array_shift($args);
+            $reason = isset($args[0]) ? $args[0] : "";
+
+            $pl->addBanToBanlog($name);
+            $sender->getServer()->getNameBans()->addBan($name, $reason, null, $sender->getName());
+            if (($player = $sender->getServer()->getPlayerExact($name)) instanceof Player) {
+                $player->kick($reason !== "" ? str_replace(["{reason}", "{line}"], [$args[0], "\n"], $cfg->get("kick-message-with-reason")) . $reason : $cfg->get("kick-message"));
+                Command::broadcastCommandMessage($sender, new TranslationContainer("%commands.ban.success", [$player !== null ? $player->getName() : $name]));
+                $sender->sendMessage("Banned!");
+            }
+        } elseif (count($args) >= 3) {
+            $name = array_shift($args);
+            $reason = isset($args[0]) ? $args[0] : "";
+
+            if (!$pl->stringToTimestamp(implode(" ", $args))) {
+                $sender->sendMessage($cfg->get("use-DateInterval-format"));
+                return true;
+            }
+
+            $informations = $pl->stringToTimestamp(implode(" ", $args));
+            $bantime = $informations[0];
+            $reason = $informations[1];
+            //if ($args[1] instanceof DateInterval) {
+            $pl->addBanToBanlog($name);
+            $sender->getServer()->getNameBans()->addBan($name, $reason, $bantime, $sender->getName());
+            if (($player = $sender->getServer()->getPlayerExact($name)) instanceof Player) {
+                $player->kick($reason !== "" ? str_replace(["{reason}", "{time}", "{line}"], [$args[0], $bantime->format("Y.m.d H:i:s"), "\n"], $cfg->get("kick-message-with-time")) . $reason : $cfg->get("kick-message"));
+                Command::broadcastCommandMessage($sender, new TranslationContainer("%commands.ban.success", [$player !== null ? $player->getName() : $name]));
+                $sender->sendMessage("[Time] Banned!");
+            }
+        } else {
+            $sender->sendMessage($this->usageMessage);
+        }
+        return true;
+    }
+}
